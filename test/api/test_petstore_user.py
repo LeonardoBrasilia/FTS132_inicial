68084784c94a3a63ff60aeffd64225ce7bf892af
Iterasys <40407006+Iterasys@users.noreply.github.com>
2021-11-23 22:21:27 -0300

    Criação dos testes da API da PetStore, entidade User, métodos Put e Delete

--- test/api/test_petstore_user.py
+++ test/api/test_petstore_user.py
@@ -65,3 +65,68 @@ def testar_consultar_usuario():
     assert corpo_da_resposta['username'] == username
     assert corpo_da_resposta['email'] == email
     assert corpo_da_resposta['phone'] == phone
+
+
+def testar_alterar_usuario():
+    # Configura
+    username = 'minduim'
+    status_code_esperado = 200
+    codigo_esperado = 200
+    tipo_esperado = 'unknown'
+    mensagem_esperada = '1001'
+
+    # Executa
+    resposta = requests.put(
+        url=f'{base_url}/{username}',
+        data=open('C:/Users/corre/PycharmProjects/fts132_inicial/test/db/user2.json', 'rb'),
+        headers=headers
+    )
+
+    # Formatação
+    corpo_da_resposta = resposta.json()
+    print(resposta)
+    print(resposta.status_code)
+    print(corpo_da_resposta)
+
+    # Validação
+    assert resposta.status_code == status_code_esperado
+    assert corpo_da_resposta['code'] == codigo_esperado
+    assert corpo_da_resposta['type'] == tipo_esperado
+    assert corpo_da_resposta['message'] == mensagem_esperada
+
+
+def testar_excluir_usuario():
+    # Configura
+    username = 'minduim'
+    status_code_esperado = 200
+    codigo_esperado = 200
+    tipo_esperado = 'unknown'
+    mensagem_esperada = 'minduim'
+
+    # Executa
+    resposta = requests.delete(
+        url=f'{base_url}/{username}',
+        headers=headers
+    )
+
+    # Formatação
+
+    match resposta.status_code:
+        case 200: # Apagar um usuário que foi encontrado
+            corpo_da_resposta = resposta.json()
+            print(resposta)
+            print(resposta.status_code)
+            print(corpo_da_resposta)
+
+            # Validação
+            assert resposta.status_code == status_code_esperado
+
+            assert corpo_da_resposta['code'] == codigo_esperado
+            assert corpo_da_resposta['type'] == tipo_esperado
+            assert corpo_da_resposta['message'] == mensagem_esperada
+
+        case 400:
+            print('username fornecido incorretamente')
+
+        case 404:
+            print('usuário não encontrado')
